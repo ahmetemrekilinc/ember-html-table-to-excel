dc6e814309c768bd7b63fd3d1acbdd5f42b18825
ahmetemrekilinc <aekilinc@gmail.com>
2019-09-02 16:52:53 +0300

    1.2.0 ember-cli is upgraded to 3.12.0

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -9,11 +9,7 @@ module.exports = function(environment) {
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       }
     },
 
